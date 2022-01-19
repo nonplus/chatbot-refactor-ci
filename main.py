5879baff65f226ba0ec17ba7d8323af267e68b59
Stepan Riha <github@nonplus.net>
2022-01-19 00:25:03 +0000

    Refactor and test

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 def respond(user_input):
     if "What is your name?" == user_input:
-        print("My name is chatbot! What's yours?")
+        return "My name is chatbot! What's yours?"
     else:
-        print("Sorry I didn't understand that.")
+        return "Sorry I didn't understand that."
 
 def main():
     while True:
         user_input = input()
-        respond(user_input)
+        response = respond(user_input)
+        print(response)
 
 if __name__ == '__main__':
     main()
